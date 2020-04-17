87b77bce5b6a2d4738b933d252d5a166aa035db8
Guillaume PREVOST <guillaume.prevost@edu.ece.fr>
2020-04-17 23:21:02 +0200

    item en attente fonctionne

--- espace_admin/admin_item.php
+++ espace_admin/admin_item.php
@@ -18,7 +18,7 @@ session_start();
 //selectionner tous les items
 $req_item_en_vente = $bdd->query('SELECT ID_item,name_item, pic1, description, categorie   FROM item WHERE etat_vente = \'en_vente\'');
 //$req_item_en_vente = $bdd->query('SELECT ID_item,name_item, pic1, description, categorie, #ID_vendeur'   FROM item WHERE etat_vente = \'en_vente\'');
-
+$req_item_en_attente = $bdd->query('SELECT ID_item,name_item, pic1, description, categorie   FROM item WHERE etat_vente = \'en_attente\'');
 $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, categorie   FROM item WHERE etat_vente = \'signalé\'');
 
 ?>
@@ -201,13 +201,15 @@ $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, ca
 
     </div><!-- col-sm-6 well ligne 72-->
 
-    <div class = "col-sm-6 well">
-      <div class = "well">
-        <h3><center><strong> Liste des items signalés</strong></center> </h3>
+    <div class = "col-sm-6 ">
+      <div class="row">
+        <div class="col-sm-12 well">
+          <div class="well">
+        <h3><center><strong> Liste des items en attente</strong></center> </h3>
       </div><!-- well ligne 73 encadré du titre-->
 
       <?php
-        while ($donnee = $req_item_signale->fetch()):
+        while ($donnee = $req_item_en_attente->fetch()):
       ?>
 
       <form class="" role ="form" method="post">
@@ -234,19 +236,19 @@ $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, ca
 
         <div class="col-sm-2">
 
-        <div>  <input type="hidden" name="keytodelete2" value="<?php echo $donnee['ID_item']; ?>" required>
-          <input type="submit" name="submitDeletebutton2" class="btn btn-black"value="Supprimer"></div>
-          <div>  <input type="hidden" name="keytoignore" value="<?php echo $donnee['ID_item']; ?>" required>
-            <input type="submit" name="submitIgnorebutton" class="btn btn-black"value="Ignorer"></div>
+        <div>  <input type="hidden" name="keytoAdd" value="<?php echo $donnee['ID_item']; ?>" required>
+          <input type="submit" name="submitAddButton" class="btn btn-black"value="Ajouter"></div>
+          <div>  <input type="hidden" name="keytoignore2" value="<?php echo $donnee['ID_item']; ?>" required>
+            <input type="submit" name="submitIgnorebutton2" class="btn btn-black"value="Ignorer"></div>
         </div>
         </form>
 
       <?php endwhile; ?>
                 <?php
 
-                        if(isset($_POST['submitDeletebutton2'])) {
-                          $id_a_supprimer=$_POST['keytodelete2'];
-                          $req_item_existe=$bdd->query("SELECT * FROM item WHERE ID_item='$id_a_supprimer'");
+                        if(isset($_POST['submitAddButton'])) {
+                          $id_a_ajouter=$_POST['keytoAdd'];
+                          $req_item_existe=$bdd->query("SELECT * FROM item WHERE ID_item='$id_a_ajouter'");
 
                           $count = $req_item_existe->rowCount();
 
@@ -256,8 +258,8 @@ $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, ca
                             $query = $bdd->prepare('UPDATE item SET etat_vente = :etat_vente WHERE ID_item = :ID_item');
 
                             $success = $query->execute(array(
-                              ':ID_item' => $id_a_supprimer,
-                              ':etat_vente' => 'banni'
+                              ':ID_item' => $id_a_ajouter,
+                              ':etat_vente' => 'en_vente'
                             ));
 
                             echo '<div class="alert alert-success"> <p>La ligne a été supprimé</p> </div>';
@@ -271,8 +273,8 @@ $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, ca
 
                         }
 
-                        if(isset($_POST['submitIgnorebutton'])) {
-                          $id_a_ignorer=$_POST['keytoignore'];
+                        if(isset($_POST['submitIgnorebutton2'])) {
+                          $id_a_ignorer=$_POST['keytoignore2'];
                           $req_item_existe=$bdd->query("SELECT * FROM item WHERE ID_item='$id_a_ignorer'");
 
                           $count = $req_item_existe->rowCount();
@@ -284,7 +286,7 @@ $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, ca
 
                             $success = $query->execute(array(
                               ':ID_item' => $id_a_ignorer,
-                              ':etat_vente' => 'en_vente'
+                              ':etat_vente' => 'banni'
                             ));
 
                             echo '<div class="alert alert-success"> <p>La ligne a été ignoré</p> </div>';
@@ -297,17 +299,116 @@ $req_item_signale = $bdd->query('SELECT ID_item,name_item, pic1, description, ca
                           }
 
                         }
+                    ?>
+                  </div>
 
+                    <div class = "col-sm-12 well">
+                      <div class="well">
+                      <h3><center><strong> Liste des items signalés</strong></center> </h3>
+                    </div><!-- well ligne 73 encadré du titre-->
 
-                  ob_end_flush();
+                    <?php
+                      while ($donnee = $req_item_signale->fetch()):
                     ?>
 
+                    <form class="" role ="form" method="post">
+                      <div class = "col-sm-3 text-center">
+                        <?php
+                         $photo=$donnee['pic1'];
+                         if(!$photo) {
+                           echo '<img src = "../vente.jpg " class="img-circle" height="80" width="80" alt="Photo"/>';
+                         }
+                         else{
+                         echo '<img src = "../img_items/'.$photo.' " class="img-circle" height="80" width="80" alt="Photo"/>';
+                       }
+                        ?>
+                      </div><!-- col-sm-3 ligne 84-->
+
+                      <div class="col-sm-7 ">
+                          <h4><strong>
+                            <?php
+                              echo $donnee['name_item']; ?>
+                          </strong></h4>
+                          <p><?php echo $donnee['categorie'];?><i>
+                              <br><?php echo $donnee['description'];?></i></p>
+                      </div><!-- col-sm-7 ligne 95-->
+
+                      <div class="col-sm-2">
+
+                      <div>  <input type="hidden" name="keytodelete2" value="<?php echo $donnee['ID_item']; ?>" required>
+                        <input type="submit" name="submitDeletebutton2" class="btn btn-black"value="Supprimer"></div>
+                        <div>  <input type="hidden" name="keytoignore" value="<?php echo $donnee['ID_item']; ?>" required>
+                          <input type="submit" name="submitIgnorebutton" class="btn btn-black"value="Ignorer"></div>
+                      </div>
+                      </form>
+
+                    <?php endwhile; ?>
+                              <?php
+
+                                      if(isset($_POST['submitDeletebutton2'])) {
+                                        $id_a_supprimer=$_POST['keytodelete2'];
+                                        $req_item_existe=$bdd->query("SELECT * FROM item WHERE ID_item='$id_a_supprimer'");
+
+                                        $count = $req_item_existe->rowCount();
+
+                                        if ($count>0) {
+
+                                        //si l'array est rempli d'au moins 1 on supprime de la liste des vendeurs autorisés
+                                          $query = $bdd->prepare('UPDATE item SET etat_vente = :etat_vente WHERE ID_item = :ID_item');
+
+                                          $success = $query->execute(array(
+                                            ':ID_item' => $id_a_supprimer,
+                                            ':etat_vente' => 'banni'
+                                          ));
+
+                                          echo '<div class="alert alert-success"> <p>La ligne a été supprimé</p> </div>';
+                                          header('Location:admin_item.php');
+
+                                        }
+                                        else {
+                                          ///warning la ligne n'existe pas
+                                          echo '<div class="alert alert-warning"> <p>La ligne n'.'existe pas</p> </div>';
+                                        }
+
+                                      }
+
+                                      if(isset($_POST['submitIgnorebutton'])) {
+                                        $id_a_ignorer=$_POST['keytoignore'];
+                                        $req_item_existe=$bdd->query("SELECT * FROM item WHERE ID_item='$id_a_ignorer'");
+
+                                        $count = $req_item_existe->rowCount();
+
+                                        if ($count>0) {
+
+                                        //si l'array est rempli d'au moins 1 on supprime de la liste des vendeurs autorisés
+                                          $query = $bdd->prepare('UPDATE item SET etat_vente = :etat_vente WHERE ID_item = :ID_item');
+
+                                          $success = $query->execute(array(
+                                            ':ID_item' => $id_a_ignorer,
+                                            ':etat_vente' => 'en_vente'
+                                          ));
+
+                                          echo '<div class="alert alert-success"> <p>La ligne a été ignoré</p> </div>';
+                                          header('Location:admin_item.php');
+
+                                        }
+                                        else {
+                                          ///warning la ligne n'existe pas
+                                          echo '<div class="alert alert-warning"> <p>La ligne n'.'existe pas</p> </div>';
+                                        }
+
+                                      }
+
+
+                                ob_end_flush();
+                                  ?>
 
 
 
+                                </div>
     </div><!-- col-sm-6 well ligne 72-->
 
-
+  </div>
      </div>
     </div>
 
