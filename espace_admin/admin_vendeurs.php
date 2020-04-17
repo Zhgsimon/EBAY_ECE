b8b8a3f1a4adfa9ae9e3d1a1cbf6368b7e671dc1
Guillaume PREVOST <guillaume.prevost@edu.ece.fr>
2020-04-17 16:56:16 +0200

    ajout du footer en mode include

--- espace_admin/admin_vendeurs.php
+++ espace_admin/admin_vendeurs.php
@@ -485,31 +485,7 @@ $req_vendeur_signale = $bdd->query('SELECT ID_user, name, First_name, login, pse
 
 
 
-<!-- Pied de page -->
-<footer class="container-fluid text-center">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-8 col-sm-12">
-      <h6><b>Information additionnelle</b></h6>
-      <p>
-        Adolf Hitler [ˈadɔlf ˈhɪtlɐ]3 Écouter est un idéologue et homme d'État allemand, né le 20 avril 1889 à Braunau am Inn en Autriche-Hongrie (aujourd'hui en Autriche et toujours ville-frontière avec l’Allemagne) et mort par suicide le 30 avril 1945 à Berlin. Fondateur et figure centrale du nazisme, il prend le pouvoir en Allemagne en 1933 et instaure une dictature totalitaire, impérialiste, antisémite et raciste désignée sous le nom de Troisième Reich.
-      </p>
-      <p>
-        L’antisémitisme est le nom donné de nos jours à la discrimination et à l'hostilité manifestées à l'encontre des Juifs
-      </p>
-    </div>
-    <div class="col-lg-4 col-md-4 col-sm-12">
-      <h6><b>Contact</b></h6>
-      <p>
-      37, quai de Grenelle, 75015 Paris, France <br>
-      info@webDynamique.ece.fr <br>
-      +33 01 02 03 04 05 <br>
-      +33 01 03 02 05 04
-      </p>
-    </div>
-  </div>
-  <div class="footer-copyright text-center">&copy; 2019 Copyright | Droit d'auteur: webDynamique.ece.fr</div>
-</footer>
+<?php include('../includes/footer.php') ?>
 
 
 </body>
