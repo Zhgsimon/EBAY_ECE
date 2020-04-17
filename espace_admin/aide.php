9f43fa0d4fff728c97d0935037d9c04ff73554ee
Guillaume PREVOST <guillaume.prevost@edu.ece.fr>
2020-04-18 00:02:10 +0200

    FAQ terminée

--- espace_admin/aide.php
+++ espace_admin/aide.php
@@ -7,10 +7,42 @@
   <link rel="stylesheet" type="text/css" href="moncompte_acheteur.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    nav{
+     background-color: _#070239 ;
+    }
+
+    body {
+      font-family: "Lato", sans-serif;
+    }
+
+    a{
+      color: black;
+    }
+
+    .alink{
+      color: black;
+    }
+
+
+
+    .btn-black{
+      background-color: #000 !important;
+      color: #fff;
+      margin-bottom: 5px ;
+      margin-right: 5px;
+      float: right;
+    }
+
+    footer{
+      background-color: #E5E4EA ;
+      color: #E5E4EA
+      padding: 60px 0 30px;
+    }
+  </style>
 </head>
 
 
-<body>
 
 <!-- //barre de navigation du haut -->
 <body>
@@ -28,7 +60,7 @@
         <ul class="nav navbar-nav" id="menu">
           <li><a href="admin_vendeurs.php">Vendeurs</a></li>
           <li><a href="admin_item.php">Item</a></li>
-          <li class="active"><a href="aide.html">Aide</a></li>
+          <li class="active"><a href="aide.php">Aide</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="../index.html"><span class="glyphicon glyphicon-log-out"></span> Quitter mon espace admin</a></li>
@@ -41,7 +73,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12 well">
-        <h1><center>F.A.Q</center></h1>
+        <h1><center><span class="glyphicon glyphicon-question-sign"></span> F.A.Q</center></h1>
       </div>
       <div class="col-sm-12 ">
         <div class="panel-group" id="accordion">
@@ -51,29 +83,61 @@
                 <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">1. Comment est organisé l'espace administrateur ?</a>
               </h4>
             </div>
-            <div id="collapseOne" class="panel-collapse collapse in">
+            <div id="collapseOne" class="panel-collapse collapse out">
               <div class="panel-body">
-                Dans l'espace administrateur, vous trouverez deux onglets:<br>- <strong>Vendeurs</strong> :gestion des vendeurs (cf question 2).
-                  <br>- <strong>Item</strong>: gestion des items (cf question 3).
+                Dans l'espace administrateur, vous trouverez deux onglets:<br>- <strong>Vendeurs</strong> :gestion des vendeurs (<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">question 2</a>).
+                  <br>- <strong>Item</strong>: gestion des items (<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">cf question 3</a>).
               </div>
             </div>
           </div>
               <div class="panel panel-default">
                 <div class="panel-heading">
                   <h4 class="panel-title">
-                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Is rezr registration required?</a>
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">2. Comment est organisé l'onglet vendeur ?</a>
                   </h4>
                 </div>
-                <div id="collapseTwo" class="panel-collapse collapse in">
+                <div id="collapseTwo" class="panel-collapse collapse out">
                   <div class="panel-body">
-                    Account registration at <strong>PrepBootstrap</strong> is only required if you will be selling or buying themes.
-                    This ensures a valid communication channel for all parties involved in any transactions.
+                  Dans cet onglet, les vendeurs sont dispersés en 3 catégories :<br>- La <strong>liste des vendeurs</strong>: ce sont les vendeurs qui peuvent actuellement
+                  vendre. Vous avez la possibilité de les <strong> supprimer</strong> en appuyant sur le bouton supprimer.<br>- Les <strong>vendeurs en attente</strong>
+                   viennent de s'inscire et attendent votre approbation pour commencer à vendre. Vous pouvez <strong>accepter </strong>ou <strong>refuser </strong>
+                  qu'ils deviennent vendeurs.<br>- Les <strong>vendeurs signalés</strong> sont des vendeurs qui ont été signalés par les acheteurs, vous pouvez <strong>
+                    ignorer ce signalement</strong> en cliquant sur ignorer ou <strong>supprimer </strong> le vendeur de la plateforme en cliquant sur supprimer.
                 </div>
               </div>
             </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">3. Comment est organisé l'onglet item ?</a>
+                </h4>
+              </div>
+              <div id="collapseThree" class="panel-collapse collapse out">
+                <div class="panel-body">
+                Dans cet onglet, les items sont dispersés en 3 catégories :<br>- La <strong>liste des items</strong>: ce sont les items qui sont actuellement
+                en vente. Vous avez la possibilité de les <strong> supprimer</strong> en appuyant sur le bouton supprimer.<br>- Les <strong>items en attente</strong>
+                viennent d'être ajoutés et ont besoin de votre approbation pour être mis en vente. Vous pouvez <strong>accepter </strong>ou <strong>refuser </strong>
+                qu'ils soient mis en vente.<br>- Les <strong>items signalés</strong> sont des items qui ont été signalés par les acheteurs, vous pouvez <strong>
+                  ignorer ce signalement</strong> en cliquant sur ignorer ou <strong>supprimer </strong> l'item de la plateforme en cliquant sur supprimer.
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">4. Comment sortir de mon espace ?</a>
+              </h4>
+            </div>
+            <div id="collapseFour" class="panel-collapse collapse out">
+              <div class="panel-body">
+            En cliquant sur <strong>"Quitter mon espace admin"</strong> vous serez redirigés vers la page d'accueil.
+            </div>
+          </div>
+        </div>
           </div>
         </div>
       </div>
+    </div>
 
 
 
