57a98b8f6f85640808a94791d7531d6ba064f4ca
Guillaume PREVOST <guillaume.prevost@edu.ece.fr>
2020-04-19 12:16:48 +0200

    requete fonctionne pas mais affichage marche

--- Achat.php
+++ Achat.php
@@ -1,5 +1,27 @@
 <?php
   session_start();
+  //historique d'achats
+  //requete pour selectionner tous les items acheté par l'acheteur
+  //boucle fetch
+
+  // connexion à la base de donnée
+    try
+    {
+      $bdd = new PDO('mysql:host=localhost;dbname=ece_ebay;charset=utf8', 'root', '',
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+
+    $ID_user=$_SESSION['ID_user']; //ID_user good
+
+  //selectionner l'historique d'achat d'un acheteur
+  $req_historique_achat = $bdd->query('SELECT *  FROM item  ' );
+  $nbr=$req_historique_achat->rowCount();
+  echo $nbr;
 ?>
 
 <?php include('includes/header.php'); ?>
@@ -20,6 +42,7 @@
      <table class="table">
      <thead>
      <tr>
+    <th>     </th>
      <th>Item</th>
      <th>Catégorie</th>
      <th>Description</th>
@@ -30,12 +53,27 @@
      </thead>
 
      <tbody>
-
-
-
-
-
-
+       <?php
+       while ($donnee = $req_historique_achat->fetch()):
+       ?>
+       <tr>
+         <td> <?php
+          $photo=$donnee['pic1'];
+            if(!$photo) {
+              echo '<img src = "img_projet/vente.jpg" class="img-circle" height="80" width="80" alt="Photo"/>';
+            }
+            else{
+            echo '<img src = "img_items/'.$photo.' " class="img-circle" height="80" width="80" alt="Photo"/>';
+          }
+         ?> </td>
+         <td><strong> <?php echo $donnee['name_item'];?></strong> </td>
+         <td> <?php echo $donnee['Categorie'];?> </td>
+         <td> <?php echo $donnee['description'];?> </td>
+         <td> <?php echo $donnee['name_item'];//faire Prix?> </td>
+         <td> <?php echo $donnee['name_item'];//faire date?> </td>
+         <td> <?php echo $donnee['ID_vendeur'];//faire vendeur?> </td>
+       </tr>
+      <?php endwhile;?>
 
 
      </tbody>
